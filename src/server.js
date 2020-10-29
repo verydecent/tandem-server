73b41d8a52a339efa7e6ce0e767e37dd57ca7ca6
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-29 14:58:56 -0700

    npm i and import cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
+// Parse JSON
 const server = express();
+
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("Hello World!");
